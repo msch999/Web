1e29b0df687e32da22ac0260c0acdca707b2a607
msch <post_msch-git@yahoo.de>
2024-10-09 02:40:27 +0200

    red cube collision detection working

--- three-load-flythroughs/three-load-flythroughs.js
+++ three-load-flythroughs/three-load-flythroughs.js
@@ -10,9 +10,9 @@ export const scene = new THREE.Scene();
 // wireframe material toggle
 const overrideMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 document.addEventListener('keydown', function (event) {
+	console.log(`Key "${event.key}" pressed [event: keydown]`);
 	// W Pressed: Toggle wireframe
 	if (event.key === 'W') {
-		//console.log(`Key "${event.key}" pressed [event: keydown]`);
 		if (scene.overrideMaterial != overrideMaterial) {
 			scene.overrideMaterial = overrideMaterial;
 		} else {
@@ -20,6 +20,18 @@ document.addEventListener('keydown', function (event) {
 		}
 		scene.material.needsUpdate = true;
 	}
+
+	var incVal = 1;
+	if (event.key === 'ArrowUp') { // up
+		redCube.position.z -= incVal;
+	} else if (event.key === 'ArrowDown') { // down
+		redCube.position.z += incVal;
+	} else if (event.key === 'ArrowLeft') { // left 
+		redCube.position.x -= incVal;
+	} else if (event.key === 'ArrowRight') { // right
+		redCube.position.x += incVal;
+	}
+
 });
 // initial wireframe material toggle
 // scene.overrideMaterial = overrideMaterial;
@@ -42,23 +54,65 @@ scene.add(gridHelper);
 
 // Kamera-Position initialisieren
 camera.position.set(0.6, 0.42, 3.86);
+camera.lookAt(0, 0, 0);
 
- // Initiate FlyControls with various params
- const controls = new FlyControls( camera, renderer.domElement );
- controls.movementSpeed = 5;
+// Initiate FlyControls with various params
+const controls = new FlyControls(camera, renderer.domElement);
+controls.movementSpeed = 5;
 //  controls.rollSpeed = Math.PI / 24;
- controls.rollSpeed = 0.5;
- controls.autoForward = false;
- controls.dragToLook = true;
- controls.keys = {};
+controls.rollSpeed = 0.5;
+controls.autoForward = false;
+controls.dragToLook = true;
+controls.keys = {};
 
- 
 // loading from glb-files 
-loadAndPlaceModels();
+// loadAndPlaceModels();
+// ###############################################################################
+// black cube
+const blackCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const blackCubeMaterial = new THREE.MeshBasicMaterial({ color: "black" });
+const blackCube = new THREE.Mesh(blackCubeGeometry, blackCubeMaterial);
+// Adding bounding box to our black box
+const blackCubeBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+blackCubeBB.setFromObject(blackCube);
+blackCube.position.set(0, 0, 0);
+scene.add(blackCube)
+
+// Red cube
+const redCubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const redCubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
+const redCube = new THREE.Mesh(redCubeGeometry, redCubeMaterial);
+// Adding bounding box to our red box
+const redCubeBB = new THREE.Box3(new THREE.Vector3(), new THREE.Vector3());
+redCubeBB.setFromObject(redCube);
+redCube.position.set(2, 0, 0);  // set position different from black cube
+scene.add(redCube)
+
+
+
+function checkCollision() {
+	if (redCubeBB.intersectsBox(blackCubeBB)) {
+		//console.log('intersected');
+		blackCube.material.transparent = true;
+		blackCube.material.opacity = 0.5;
+		blackCube.material.color = new THREE.Color(Math.random * 0xffffff);
+	} else {
+		blackCube.material.opacity = 1;
+	}
+}
+// ###############################################################################
+
+
+// ###############################################################################
 
 function animate() {
+	redCubeBB
+		.copy(redCube.geometry.boundingBox)
+		.applyMatrix4(redCube.matrixWorld);
 
-	// update controls with a small step value to "power its engines"
+		checkCollision();
+
+	// update fly controls with a small step value to "power its engines"
 	controls.update(0.01)
 
 	// controlsGui.update();
